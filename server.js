4341c83bc6f140bed346cc1e703f54a94cfa21d1
rrdkent <rrdkent@gmail.com>
2015-10-06 01:40:27 +0000

    Added nickname greeting

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.get('/nickname', function(request,response){
+  response.send('Hello talented eagle')  
+});
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656! I am a complete ninja now!');
 });
 
 app.listen(process.env.PORT || 4000);

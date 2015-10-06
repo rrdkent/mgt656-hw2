321df06a4d5a30592125b792ed3e86f5fee44d5d
rrdkent <rrdkent@gmail.com>
2015-10-06 01:49:18 +0000

    truncated nickname call

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/nickname', function(request,response){
-  response.send('Hello talented eagle')  
+  response.send('talented eagle')  
 });
 app.get('/', function(request, response){
     response.send('Hello MGT-656! I am a complete ninja now!');
